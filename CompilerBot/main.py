36523ee13508fa13e26288309a5765f79f7077d2
sungatullina gulnara gabdullovna <sungulnara2000@mail.ru>
2019-05-12 20:39:36 +0300

    update readme

--- CompilerBot/main.py
+++ CompilerBot/main.py
@@ -6,20 +6,35 @@ URL = "https://api.jdoodle.com/v1/execute"
 
 bot = telebot.TeleBot('886490740:AAHCNylo55qhlWGcyiJ4fRe_u1Ov2AoK01M')
 
+class Program:
+    code = ''
+    input = ''
+    language = ''
+    def __init__(self, language):
+        self.language = language
+
+program = Program('cpp')
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет!')
 
+@bot.message_handler(commands=['code'])
+def get_code(message):
+    bot.send_message(message.chat.id, 'Пришли мне свой код!')
+
+
 @bot.message_handler(content_types=['text'])
 def send_text(message):
         print(message.text)
-        PARAMS = {"script": message.text,
+        params = {"script": message.text,
                   "language": "cpp",
                   "versionIndex": "0",
+                  # "stdin":
                   "clientId": "572a7d3d9dcafa3d2e7596e710656c70",
                   "clientSecret": "806d6ae46c4fc99e82c259b196c9ae0ee53ebf91ac0919519ad07cc9308dc1ab"}
-        print(PARAMS)
-        r = requests.post(url=URL, json=PARAMS, verify=False)
+        print(params)
+        r = requests.post(url=URL, json=params)
         data = r.json()
         bot.send_message(message.chat.id, data['output'])
 
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # CompilerBot
-2nd rewiew in python course. Second MIPT GIHT term.
+2nd rewiew in python course. Second MIPT DIHT term.
+
+Телеграм-бот запущен на инстансе aws. Его можно найти по ссылке: t.me/CppCompilerBot.
+token: 886490740:AAHCNylo55qhlWGcyiJ4fRe_u1Ov2AoK01M
+
+Как запустить код?
+-git clone https://github.com/sungulnara2000/CompilerBot.git compiler_bot
+-pip3 install telebot
+-cd compiler_bot
+-python3 CompilerBot/main.py
+
